73aefcf69a394146be6f9800df363348682124a5
emma <emmareeb@gmail.com>
2025-05-19 12:57:55 +0200

    pump succesfully connected

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <Arduino.h>
+
+
+moistureSensor pin(18);
+LED pin(26)
+
 void setup() {
 // write your initialization code here
+
+    Serial.begin(9600);
+
+pinMode(LED, OUTPUT);
+pinMode(moistureSensor, INPUT);
+
+digitalWrite(LED, LOW);
+
 }
 
 void loop() {
