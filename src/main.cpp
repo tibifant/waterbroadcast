9b8ff1d4c80800490a4554636b6b82f265b7e922
emma <emmareeb@gmail.com>
2025-05-19 13:42:04 +0200

    added RTOS code refactored, connected to üump

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,60 @@
 #include <Arduino.h>
 
+int sensorPin = 34;
+int pumpPin = 19;
 
-moistureSensor pin(18);
-LED pin(26)
+
+int sensorValue = 0;
 
 void setup() {
-// write your initialization code here
 
-    Serial.begin(9600);
+  Serial.begin(9600);
 
-pinMode(LED, OUTPUT);
-pinMode(moistureSensor, INPUT);
+  pinMode(sensorPin, INPUT);
+  pinMode(pumpPin, OUTPUT);
+  digitalWrite(LED, LOW);
 
-digitalWrite(LED, LOW);
+xtaskCreatePinnedToCore(
+    taskPump,   /* Function to implement the task */
+    "TaskPump", /* Name of the task */
+    10000,      /* Stack size in bytes */
+    NULL,       /* Task input parameter */
+    1,          /* Priority of the task */
+    NULL,       /* Task handle. */
+    0);         /* Core where the task should run */
 
+  Serial.println("Setup complete");
+}
+
+void taskPump(void *parameter) {
+
+
+  for (;;) {
+    sensorValue = analogRead(sensorPin);
+
+    Serial.print("Soil Moisture Value: ");
+    Serial.println(sensorValue);
+
+    if (sensorValue > 500) {
+
+      digitalWrite(pumpPin, HIGH)
+      vTaskDelay(100000 / portTICK_PERIOD_MS);
+      digitalWrite(pumpPin, LOW)
+
+    } else {
+
+      digitalWrite(pumpPin, LOW);
+    }
+
+
+  }
+
+    Serial.println("Task running");
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
 }
 
 void loop() {
-// write your code here
+
+
 }
